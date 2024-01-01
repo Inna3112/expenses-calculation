72752ace5f863be5fd46b62737f7c9c5038309e5
Inna3112 <innula3112@gmail.com>
2024-01-01 18:27:30 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default App;
---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>LAst 7 days</Text>
+      <Text>$122.89</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
